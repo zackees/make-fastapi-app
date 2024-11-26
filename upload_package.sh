9595099d6a04f3f4a71c4edc411318660ddc8b3a
zackees <z@zackees.com>
2024-11-25 17:47:33 -0800

    chore: add activation and installation scripts for virtual environment management

--- upload_package.sh
+++ upload_package.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 rm -rf build dist
-. ./activate.sh
-pip install wheel twine
+. ./activate
+pip install wheel twine setuptools --upgrade
 echo "Building Source and Wheel (universal) distribution…"
 python setup.py sdist bdist_wheel --universal
 echo "Uploading the package to PyPI via Twine…"
